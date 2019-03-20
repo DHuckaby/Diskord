cb5736aaa2323030d04e90ba37bface2384523c9
Jesse Corbett <jesse.corbett@springventuregroup.com>
2019-03-20 16:18:57 -0500

    Add javascript support

--- src/jvmMain/kotlin/com/jessecorbett/diskord/internal/Time.kt
+++ src/jvmMain/kotlin/com/jessecorbett/diskord/internal/Time.kt
@@ -7,5 +7,5 @@ internal actual fun epochSecondNow(): Long = Instant.now().epochSecond
 
 internal actual fun epochMillisNow(): Long = Instant.now().toEpochMilli()
 
-internal actual fun parseRfc1123(timestamp: String): Long =
+internal actual fun parseRfc1123Seconds(timestamp: String): Long =
     DateTimeFormatter.RFC_1123_DATE_TIME.parse(timestamp, Instant::from).epochSecond
